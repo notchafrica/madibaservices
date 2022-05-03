b7a03afb5fc160917c08dd74e30de86cc5305a30
chapdel <drop@chapdel.me>
2022-05-03 20:53:29 +0100

    Added livewire

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,13 @@
+<header>
+    <div class="flex">
+        <div class="flex-none w-14 h-14 ...">
+          01
+        </div>
+        <div class="grow h-14 bg-red-500">
+          02
+        </div>
+        <div class="flex-none w-14 h-14 ...">
+          03
+        </div>
+      </div>
+</header>
